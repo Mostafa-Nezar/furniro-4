acc8f85ef8d1d97ed8376c75ccbf3a408aee0558
Mostafa Nezar <mostafanezar19@gmail.com>
2024-10-17 03:10:32 +0300

    eight 8 6

--- furniro.js
+++
@@ -1,4 +0,0 @@
-import { createproduct, likeitem, myproducts } from "./main/products.js";
-const limitedProducts = myproducts.slice(0, 8);
-createproduct(document.getElementById("products-show"), limitedProducts);
-likeitem(document.getElementById("products-show"), limitedProducts, myproducts);
